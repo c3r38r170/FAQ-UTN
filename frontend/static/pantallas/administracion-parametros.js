ff0867fcac8e1894c5d39a77f6d2289061ff928b
Matias Bais <matiasbais1998@gmail.com>
2024-02-23 17:48:12 -0300

    fix notificaciones en administracion/parametros

--- frontend/static/pantallas/administracion-parametros.js
+++ frontend/static/pantallas/administracion-parametros.js
@@ -9,10 +9,10 @@ function crearPantalla(ruta,sesion, p){
         titulo: '',
         sesion: sesion
     });
-    const textoEntradas = "Resultados por página. <br><font size='2'>Especifíca cuantos resultados vienen en cada página. Con un número más alto tarda más en cargar, con un numero más bajo tarda menos a costa de mas peticiones al servidor. <br> Valor actual: " + p.EntradasPorPagina + " <font>";
-    const textoModera = "Si se modera por IA. <br><font size='2'>Especifíca si al un usuario generar contenido, este pasa por un primer filtro por IA. <br> Valor actual: " +( p.ModerarIA ? "Sí": "No" )+ " <font>";
-    const textoSinReporte = "Margen de confianza para aceptar post. <br><font size='2'>Especifíca el porcentaje de confianza mínimo que debe tener la IA para aceptar un post. <br> Valor actual: " + p.ReportaPost + " <font>";
-    const textoConReporte = "Margen de confianza para aceptar post con reporte. <br><font size='2'> Post con un nivel de confianza entre este valor y el para aceptar post serán aceptados pero automáticamente se creará un reporte. Con un valor menor serán rechazados. <br> Valor actual: " + p.RechazaPost + " <font>";
+    const textoEntradas = "Resultados por página. <br><font size='2'>Especifíca cuantos resultados vienen en cada página. Con un número más alto tarda más en cargar, con un numero más bajo tarda menos a costa de mas peticiones al servidor. <br> Valor actual: " + p.EntradasPorPagina + " </font>";
+    const textoModera = "Si se modera por IA. <br><font size='2'>Especifíca si al un usuario generar contenido, este pasa por un primer filtro por IA. <br> Valor actual: " +( p.ModerarIA ? "Sí": "No" )+ " </font>";
+    const textoSinReporte = "Margen de confianza para aceptar post. <br><font size='2'>Especifíca el porcentaje de confianza mínimo que debe tener la IA para aceptar un post. <br> Valor actual: " + p.ReportaPost + " </font>";
+    const textoConReporte = "Margen de confianza para aceptar post con reporte. <br><font size='2'> Post con un nivel de confianza entre este valor y el para aceptar post serán aceptados pero automáticamente se creará un reporte. Con un valor menor serán rechazados. <br> Valor actual: " + p.RechazaPost + " </font>";
     
     let modal = new Modal('General','modal-general');
     pagina.partes.push(modal);
