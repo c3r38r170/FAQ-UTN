6da30ec89ba497470b4df3c2974b438ef45449d2
Matias Bais <matiasbais1998@gmail.com>
2024-02-27 18:53:59 -0300

    color chip usuario fuera del perfil

--- frontend/static/componentes/chipusuario.js
+++ frontend/static/componentes/chipusuario.js
@@ -1,36 +1,32 @@
-import { Fecha } from "./fecha.js"
+import { Fecha } from "./fecha.js";
 
 // TODO Refactor: Propiedades como CORTO o LARGO de fecha, pero para si es completo o simple (u otro par de adjetivos)
-class ChipUsuario{
-    #DNI
-    #nombreusuario;
-    #correo
-    #createdAt
-    #esPerfil;
-    #tipo;
-    #color;
+class ChipUsuario {
+  #DNI;
+  #nombreusuario;
+  #correo;
+  #createdAt;
+  #esPerfil;
+  #tipo;
+  #color;
 
-	constructor({
-        DNI
-        ,nombre
-        ,correo
-        ,createdAt
-        ,fecha_alta
-        ,perfil
-    },esPerfil=false){
-        this.#DNI = DNI;
-		this.#nombreusuario = nombre;
-        this.#correo = correo;
-        this.#createdAt = new Fecha(fecha_alta || createdAt);
-        this.#esPerfil = esPerfil;
-        this.#tipo =perfil ? perfil.nombre:"Usuario";
-        this.#color = perfil? perfil.color :"#485fc7"
-	}
-	render(){
-        //TODO Feature Tipo de Usuario
+  constructor(
+    { DNI, nombre, correo, createdAt, fecha_alta, perfil },
+    esPerfil = false
+  ) {
+    this.#DNI = DNI;
+    this.#nombreusuario = nombre;
+    this.#correo = correo;
+    this.#createdAt = new Fecha(fecha_alta || createdAt);
+    this.#esPerfil = esPerfil;
+    this.#tipo = perfil ? perfil.nombre : "Usuario";
+    this.#color = perfil ? perfil.color : "#485fc7";
+  }
+  render() {
+    //TODO Feature Tipo de Usuario
 
-        if(this.#esPerfil){
-            return`
+    if (this.#esPerfil) {
+      return `
             <div class="chip-usuario-perfil">
                 <img class="mr-3 img-usuario" src="../user.webp" ></img>
                 <div class="contenido-perfil">
@@ -38,20 +34,26 @@ class ChipUsuario{
                     <div>Nombre: <span>${this.#nombreusuario}</span></div>
                     <div>Correo electrónico: <span>${this.#correo}</span></div>
                     <div>Miembro desde: <span>${this.#createdAt.render()}</span></div>
-                    <div class="tipo-usuario" style="background-color: ${this.#color}">${this.#tipo}</div>
+                    <div class="tipo-usuario" style="background-color: ${
+                      this.#color
+                    }">${this.#tipo}</div>
                 </div>
             </div>
             `;
-        }else{
-            return`
+    } else {
+      return `
             <div class="chip-usuario is-vcentered">
                 <img class="mr-3 img-usuario" src="../user.webp" ></img>
-                <a class="nombre-usuario" href="/perfil/${this.#DNI}">${this.#nombreusuario}</a>
-                <div class="tipo-usuario">${this.#tipo}</div>
+                <a class="nombre-usuario" href="/perfil/${this.#DNI}">${
+        this.#nombreusuario
+      }</a>
+                <div class="tipo-usuario" style="background-color: ${
+                  this.#color
+                }">${this.#tipo}</div>
             </div>
             `;
-        }
-	}
+    }
+  }
 }
 
-export {ChipUsuario};
+export { ChipUsuario };
