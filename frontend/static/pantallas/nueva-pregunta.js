c56f658adb46bb0b4f7b7cfdd9feb0dab1d25b5c
c3r38r170 <c3r38r170@gmail.com>
2024-03-06 16:09:09 -0300

    Limpieza y actualización de comentarios en pantallas/nueva-pregunta.js

--- frontend/static/pantallas/nueva-pregunta.js
+++ frontend/static/pantallas/nueva-pregunta.js
@@ -1,5 +1,4 @@
-import { Pagina, Formulario, ComponenteLiteral } from "../componentes/todos.js";
-
+import { Pagina, Formulario} from "../componentes/todos.js";
 
 // TODO refactor: Usar campo de Lista
 function crearPagina(ruta,sesion,categorias){
@@ -29,12 +28,11 @@ function crearPagina(ruta,sesion,categorias){
 					textoEnviar:'Crear Pregunta', clasesBoton:'is-link is-rounded mt-3'
 				}
 			)
-			// ,new ComponenteLiteral(()=>'<label class=label>Sugerencias basadas en el título:</label>')
 			// TODO Feature: Formulario de creación de preguntas 
 			// ✅ Campo de Título. Tiene que sugerir preguntar relacionadas. 
 			// ✅ Campo de etiquetas. Se deben obtener las etiquetas, mostrarlas, permitir elegirlas.
 			// ✅ Campo de cuerpo. Texto largo con un máximo y ya.
-			// Las sugerencias pueden ser un panel abajo, o abajo del título... que se vaya actualizando a medida que se escribe el cuerpo.
+			// ✅ Las sugerencias pueden ser un panel abajo, o abajo del título... que se vaya actualizando a medida que se escribe el cuerpo.
 			// Botón de crear pregunta. Se bloquea, si hay un error salta cartel (como por moderación), si no lleva a la página de la pregunta. Reemplaza, así volver para atrás va al inicio y no a la creación de preguntas.
 		]
 	});
