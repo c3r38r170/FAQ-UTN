7323900fa4002cd866823288f8d426f9d45de8e5
Matias Bais <matiasbais1998@gmail.com>
2024-03-15 09:32:47 -0300

    cambiada foto notificacion

--- frontend/static/componentes/notificacion.js
+++ frontend/static/componentes/notificacion.js
@@ -1,8 +1,8 @@
-import {Fecha} from './fecha.js';
+import { Fecha } from './fecha.js';
 
-class Notificacion{
+class Notificacion {
 	#texto;
-    #tituloPregunta;
+	#tituloPregunta;
 	visto;
 	#tipo;
 	#fecha;
@@ -21,66 +21,66 @@ class Notificacion{
 		pregunta
 	*/
 
-	constructor(id,{
-	    visto
-			,createdAt
-			,cantidad
-			,propia
-			,titulo
-			,respuestaPreguntaID
-			,preguntaID
-    },usuarioActualDNI){
-			this.visto=visto;
-			this.#ID=id;
+	constructor(id, {
+		visto
+		, createdAt
+		, cantidad
+		, propia
+		, titulo
+		, respuestaPreguntaID
+		, preguntaID
+	}, usuarioActualDNI) {
+		this.visto = visto;
+		this.#ID = id;
 		this.#tituloPregunta = titulo;
-		this.#fecha=new Fecha(createdAt);
-		if(preguntaID){
-			this.#idPregunta =preguntaID
-			if(propia){
+		this.#fecha = new Fecha(createdAt);
+		if (preguntaID) {
+			this.#idPregunta = preguntaID
+			if (propia) {
 				this.#texto = 'Nuevos votos positivos en tu pregunta:'
-			}else{
-				this.#texto= 'Nueva pregunta que te puede interesar:'
+			} else {
+				this.#texto = 'Nueva pregunta que te puede interesar:'
 			}
-		}else{
-			this.#idPregunta=respuestaPreguntaID
-			if(propia){
+		} else {
+			this.#idPregunta = respuestaPreguntaID
+			if (propia) {
 				this.#texto = 'Fuiste votado respondiendo a:'
-			}else{
+			} else {
 				this.#texto = 'Nuevas respuestas en la pregunta'
 			}
 		}
 	}
 
-	static verNotificacion(e){
+	static verNotificacion(e) {
 		e.preventDefault();
-	let aApretado = e.target.closest("a");
-	let divChipNotificacion = aApretado.closest(".notificacion");
-	let id = divChipNotificacion.dataset.id;
-	let idPregunta = divChipNotificacion.dataset.idPregunta;
-	
-	
-	const url= `http://localhost:8080/api/notificacion`;
-	// TODO Refactor: Ver si esto choca o es equivalente al hecho de que las notificaciones se ven al entrar en la página.
-	fetch(url, {
-		method: 'PATCH',
-		headers: {
-			'Content-Type': 'application/json'
-		},
-		body: JSON.stringify({
-			ID: id 
+		let aApretado = e.target.closest("a");
+		let divChipNotificacion = aApretado.closest(".notificacion");
+		let id = divChipNotificacion.dataset.id;
+		let idPregunta = divChipNotificacion.dataset.idPregunta;
+
+
+		const url = `http://localhost:8080/api/notificacion`;
+		// TODO Refactor: Ver si esto choca o es equivalente al hecho de que las notificaciones se ven al entrar en la página.
+		fetch(url, {
+			method: 'PATCH',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				ID: id
+			})
+		}).then(response => {
+			window.location.replace('/pregunta/' + idPregunta)
 		})
-	}).then(response=>{
-		window.location.replace('/pregunta/'+idPregunta)
-	})
 
 	}
 
-	render(){
+	render() {
 		// TODO Feature: Matar el user.webp viejo, meter acá el endpoint de imagen de usuario.
-		return`
-		<div class="chip-notificacion notificacion ${this.visto==0? 'noti-no-vista': ''}" id='chip-notificacion-${this.#ID}' data-id='${this.#ID}' data-idPregunta='${this.#idPregunta}'>
+		return `
+		<div class="chip-notificacion notificacion ${this.visto == 0 ? 'noti-no-vista' : ''}" id='chip-notificacion-${this.#ID}' data-id='${this.#ID}' data-idPregunta='${this.#idPregunta}'>
 			<div class="img-container">
-				<img class="img" class="img" src="/user.webp"/>
+				<img class="img" class="img" src="${this.visto == 0 ? "/no-vista.png" : "/vista.png"}"/>
 			</div>
 			<div class="noti-container">
 			${this.#texto} <a class="notificacion" href="/pregunta/${this.#idPregunta}">${this.#tituloPregunta}</a>
@@ -91,4 +91,4 @@ class Notificacion{
 	}
 }
 
-export {Notificacion};
+export { Notificacion };
