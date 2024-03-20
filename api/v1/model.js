3d53dfe24637f31f89768164a0dce1e4aa9370e9
c3r38r170 <c3r38r170@gmail.com>
2024-03-20 18:19:22 -0300

    Fix merge suscripciones.

--- api/v1/model.js
+++ api/v1/model.js
@@ -920,9 +920,9 @@ Pregunta.pagina = ({ pagina = 0, duenioID: duenioDNI, filtrar, formatoCorto, usu
           }
         };
         
-        if(filtrar?.suscripto){
+        if(filtrar?.suscripciones){
           opcionesSuscripciones.where.suscriptoDNI=usuarioActual.DNI;
-          opcionesSuscripciones.required=true;
+          // opcionesSuscripciones.required=true;
         }else{
           opcionesSuscripciones.include={ model: Usuario, as: 'suscripto', where: {DNI:usuarioActual.DNI} };
           opcionesSuscripciones.required=false;
