02116a164c043ccbe50f690b8da2c44acf7d2d33
Nicolás Fierro <nico.fierro1@gmail.com>
2024-03-25 15:53:38 -0300

    Borrar / Restaurar post en pagina individual de pregunta

--- frontend/router.js
+++ frontend/router.js
@@ -15,7 +15,8 @@ import {
   MensajeInterfaz,
   Titulo,
   Formulario,
-  Desplegable
+  Desplegable,
+  ComponenteLiteral
 } from "./static/componentes/todos.js";
 import {
   Voto as VotoDAO,
@@ -93,8 +94,6 @@ router.get("/", (req, res) => {
 
 // * Ruta que muestra 1 pregunta con sus respuestas
 router.get("/pregunta/:id?", async (req, res) => {
-  // TODO Feature: En caso de que sea una pregunta borrada, no permitir a menos que se tengan permisos de moderación, o administración.
-
   try {
     let paginaError = SinPermisos(req.session, "Algo ha malido sal.");
     if (req.params.id) {
@@ -200,6 +199,8 @@ router.get("/pregunta/:id?", async (req, res) => {
             not.save();
           }
         });
+
+
       } else if (p.post.eliminadorDNI) {
         // No está logueado y la pregunta esta eliminada
         res.send(paginaError.render());
@@ -226,14 +227,12 @@ router.get("/pregunta/:id?", async (req, res) => {
       });
 
       // TODO UX: Esto no se ve muy lindo. Alternativa: Alguna forma de que la pregunta no renderice el link, y sí renderice un título h-
-      let preguntaID = p.ID;
-      let pagina = PaginaPregunta(req.path, req.session, preguntaID);
+      // let preguntaID = p.ID;
+      let pagina = PaginaPregunta(req.path, req.session, p);
       pagina.titulo = p.titulo;
       p.titulo = "";
-      pagina.partes.unshift(new Pregunta(p, pagina.partes[0], req.session.usuario));
 
-      pagina.globales.preguntaID = preguntaID;
-
+      pagina.globales.pregunta = p;
       res.send(pagina.render());
     } else {
       let sesion = req.session;
--- frontend/static/pantallas/pregunta.js
+++ frontend/static/pantallas/pregunta.js
@@ -1,8 +1,6 @@
-import { Modal } from "../componentes/modal.js";
-import { Pagina } from "../componentes/pagina.js";
-import { Formulario } from "../componentes/formulario.js";
+import { Pregunta, Modal, Pagina, Formulario, ComponenteLiteral, MensajeInterfaz} from "../componentes/todos.js"
 
-function PaginaPregunta(ruta, sesion, idPregunta){
+function PaginaPregunta(ruta, sesion, pregunta){
     
     let pagina = new Pagina({
         ruta: ruta,
@@ -11,14 +9,39 @@ function PaginaPregunta(ruta, sesion, idPregunta){
     });
 
     let modal = new Modal('General','modal-general');
-    pagina.partes.push(modal);
+    
+    let posibleMensajePostBloqueado;
+    let posibleBotonModerar;
+    if(sesion.usuario && sesion.usuario.perfil.permiso.ID > 1){
+        let boton;
+        if(pregunta.post.eliminadorDNI){
+            boton = `<button id="botonRestaurar" data-ID="${pregunta.ID}" type="button" class="button is-warning is-light is-small is-rounded is-outlined">Restaurar Post</button>`
+            posibleMensajePostBloqueado = new MensajeInterfaz(MensajeInterfaz.GRIS,'<i class="fa-solid fa-ban mr-2"></i> Este Post se encuentra borrado')
+        }else{
+            boton = `<button id="botonBorrar" data-ID="${pregunta.ID}" type="button" class="button is-warning is-small is-rounded">Borrar Post</button>`
+            posibleMensajePostBloqueado = new ComponenteLiteral(()=> ``)
+        }
+        posibleBotonModerar = new ComponenteLiteral(()=> `<div class="contenedor-boton-bloqueo">${boton}</div> `)
+    }else{
+        posibleMensajePostBloqueado = new ComponenteLiteral(()=> ``)
+        posibleBotonModerar = new ComponenteLiteral(()=> ``)
+    };
+
+
+    pagina.partes.push(
+        modal,
+        posibleMensajePostBloqueado,
+        posibleBotonModerar,
+        new Pregunta(pregunta, modal, sesion.usuario)
+        );
+
     if(sesion && sesion.usuario){
         let form = new Formulario(
             'nueva-respuesta'
             ,'/api/respuesta'
             ,[
                 {name:'cuerpo',textoEtiqueta:'Tu respuesta:',type:'textarea'},
-                {name:'IDPregunta', textoEtiqueta:'idPregunta', type:'hidden', value:idPregunta}
+                {name:'IDPregunta', textoEtiqueta:'idPregunta', type:'hidden', value:pregunta.ID}
             ]
             ,(respuesta,{ok,codigo})=>{
                 if(ok)
@@ -34,7 +57,9 @@ function PaginaPregunta(ruta, sesion, idPregunta){
                 // ,alEnviar:()=>document.getElementById()
             }
         )
-        pagina.partes.push(form)
+        pagina.partes.push(
+            form
+            )
     }
 
     return pagina;
